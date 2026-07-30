2ae5daf7e86127f8d513bdead1e7d13ad51a2352
Shuaicheng Zhu <jason23347@gmail.com>
2026-07-30 15:56:25 +0800

    Update USTB Web Login to version 1.1.0, adding support for multiple accounts with a default account selection. Refactor credential handling in the login script and enhance the configuration file structure.

--- package/luci-app-ustb-login/luasrc/model/cbi/ustb_login.lua
+++ package/luci-app-ustb-login/luasrc/model/cbi/ustb_login.lua
@@ -5,14 +5,28 @@ local uci = require "luci.model.uci".cursor()
 local m = Map("ustb_login", translate("USTB Web Login"),
     translate("Configure USTB web login."))
 
+-- 全局登录设置
 local s = m:section(TypedSection, "ustb_login", translate("Login Settings"))
 s.anonymous = true
+s.addremove = false
 
 s:option(Flag, "enabled", translate("Enable"))
 
-s:option(Value, "username", translate("Username"))
-local pw = s:option(Value, "password", translate("Password"))
-pw.password = true
+local da = s:option(ListValue, "default_account", translate("Default Account"))
+da.rmempty = true
+da:value("", translate("-- Please choose --"))
+uci:foreach("ustb_login", "account", function(sec)
+	local sid = sec[".name"]
+	local remark = sec.remark or ""
+	local user = sec.username or ""
+	if remark ~= "" then
+		da:value(sid, remark)
+	elseif user ~= "" then
+		da:value(sid, user)
+	else
+		da:value(sid, sid)
+	end
+end)
 
 s:option(Value, "login_host", translate("Login Host"))
 s:option(Flag, "attempt_ipv6", translate("Attempt IPv6"))
@@ -20,38 +34,48 @@ s:option(Value, "default_ipv6_address", translate("Default IPv6 Address"))
 
 -- 自动获取并显示 IPv6（只读）
 do
-  local dv = s:option(DummyValue, "_detected_ipv6", translate("Detected IPv6"))
-  function dv.cfgvalue(self, sid)
-    local attempt_ipv6 = tonumber(uci:get("ustb_login", sid, "attempt_ipv6") or "0")
-    if attempt_ipv6 < 1 then
-      return translate("Disabled")
-    end
+	local dv = s:option(DummyValue, "_detected_ipv6", translate("Detected IPv6"))
+	function dv.cfgvalue(self, sid)
+		local attempt_ipv6 = tonumber(uci:get("ustb_login", sid, "attempt_ipv6") or "0")
+		if attempt_ipv6 < 1 then
+			return translate("Disabled")
+		end
 
-    local def = uci:get("ustb_login", sid, "default_ipv6_address") or ""
-    if def ~= "" then
-      return def
-    end
+		local def = uci:get("ustb_login", sid, "default_ipv6_address") or ""
+		if def ~= "" then
+			return def
+		end
 
-    -- 调用 uclient-fetch 获取内容
-    local body = util.trim(util.exec("uclient-fetch -qO- http://cippv6.ustb.edu.cn/get_ip.php 2>/dev/null"))
-    if body == "" then
-      return translate("Unavailable")
-    end
+		local body = util.trim(util.exec("uclient-fetch -qO- http://cippv6.ustb.edu.cn/get_ip.php 2>/dev/null"))
+		if body == "" then
+			return translate("Unavailable")
+		end
 
-    local ip = body:match("gIpV6Addr%s*=%s*'([^']+)'") or ""
-    return (ip ~= "" and ip) or translate("Unavailable")
-  end
+		local ip = body:match("gIpV6Addr%s*=%s*'([^']+)'") or ""
+		return (ip ~= "" and ip) or translate("Unavailable")
+	end
 end
 
 -- 手动登录按钮
 do
-  local btn = s:option(Button, "_manual_login", translate("Manual Login"))
-  btn.inputtitle = translate("Login now")
-  btn.inputstyle = "apply"
-  function btn.write(self, sid)
-    -- 后台执行，避免阻塞页面
-    sys.call("/usr/bin/ustb_login.lua >/dev/null 2>&1 &")
-  end
+	local btn = s:option(Button, "_manual_login", translate("Manual Login"))
+	btn.inputtitle = translate("Login now")
+	btn.inputstyle = "apply"
+	function btn.write(self, sid)
+		sys.call("/usr/bin/ustb_login.lua >/dev/null 2>&1 &")
+	end
 end
+
+-- 多账号管理
+local a = m:section(TypedSection, "account", translate("Accounts"),
+    translate("Add multiple accounts and select one as default above."))
+a.anonymous = true
+a.addremove = true
+a.template = "cbi/tblsection"
+
+a:option(Value, "remark", translate("Remark"))
+a:option(Value, "username", translate("Username"))
+local pw = a:option(Value, "password", translate("Password"))
+pw.password = true
 
 return m
--- package/luci-app-ustb-login/root/usr/bin/ustb_login.lua
+++ package/luci-app-ustb-login/root/usr/bin/ustb_login.lua
@@ -7,41 +7,74 @@ local ltn12 = require "ltn12"
 local enabled = uci:get("ustb_login", "main", "enabled")
 if enabled ~= "1" then os.exit(0) end
 
-local username = uci:get("ustb_login", "main", "username") or ""
-local password = uci:get("ustb_login", "main", "password") or ""
+local function resolve_credentials()
+	local default_account = uci:get("ustb_login", "main", "default_account") or ""
+
+	if default_account ~= "" then
+		local username = uci:get("ustb_login", default_account, "username")
+		local password = uci:get("ustb_login", default_account, "password")
+		if username and username ~= "" then
+			return username, password or ""
+		end
+	end
+
+	-- 回退：使用第一个账号
+	local first_user, first_pass
+	uci:foreach("ustb_login", "account", function(sec)
+		if not first_user and sec.username and sec.username ~= "" then
+			first_user = sec.username
+			first_pass = sec.password or ""
+			return false
+		end
+	end)
+	if first_user then
+		return first_user, first_pass
+	end
+
+	-- 兼容旧配置：凭据仍在 main 中
+	return uci:get("ustb_login", "main", "username") or "",
+	       uci:get("ustb_login", "main", "password") or ""
+end
+
+local username, password = resolve_credentials()
 local login_host = uci:get("ustb_login", "main", "login_host") or ""
 local attempt_ipv6 = tonumber(uci:get("ustb_login", "main", "attempt_ipv6") or "0")
 local default_ipv6 = uci:get("ustb_login", "main", "default_ipv6_address") or ""
 
+if username == "" or login_host == "" then
+	os.execute('logger -t ustb_login "No account configured, skip login."')
+	os.exit(0)
+end
+
 -- 获取IPv6
 local ip_addr = ""
 if attempt_ipv6 >= 1 then
-    if default_ipv6 ~= "" then
-        ip_addr = default_ipv6
-    else
-        local resp = {}
-        http.request{
-            url = "http://cippv6.ustb.edu.cn/get_ip.php",
-            sink = ltn12.sink.table(resp)
-        }
-        local body = table.concat(resp)
-        ip_addr = body:match("gIpV6Addr%s*=%s*'([^']+)'") or ""
-    end
+	if default_ipv6 ~= "" then
+		ip_addr = default_ipv6
+	else
+		local resp = {}
+		http.request{
+			url = "http://cippv6.ustb.edu.cn/get_ip.php",
+			sink = ltn12.sink.table(resp)
+		}
+		local body = table.concat(resp)
+		ip_addr = body:match("gIpV6Addr%s*=%s*'([^']+)'") or ""
+	end
 end
 
 -- 拼接参数
 local params = string.format("callback=suibian&DDDDD=%s&upass=%s&0MKKey=123456&v6ip=%s",
-    username, password, ip_addr)
+	username, password, ip_addr)
 
 local resp = {}
 http.request{
-    url = login_host .. "/drcom/login?" .. params,
-    sink = ltn12.sink.table(resp)
+	url = login_host .. "/drcom/login?" .. params,
+	sink = ltn12.sink.table(resp)
 }
 local body = table.concat(resp)
 
 if body:match('"result":1') then
-    os.execute('logger -t ustb_login "Login succeed."')
+	os.execute(string.format('logger -t ustb_login "Login succeed as %s."', username))
 else
-    os.execute('logger -t ustb_login "Login failed."')
+	os.execute(string.format('logger -t ustb_login "Login failed as %s."', username))
 end
